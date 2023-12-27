97229096f21de68e5e6f3c399da33cd177cd2f2e
Nanhumly <admin@warbler.cc>
2023-12-27 10:21:28 +0000

    msm8998-common: Remove duplicate SystemUI optimizations
    * They always exist in vendor/lineage/config/common.mk
      This also reverts commit 932e54670 and commit d4db9354a
    
    Change-Id: I59bff3e6e5f6961c7153dcda8f5125ad248fb87e

--- msm8998.mk
+++ msm8998.mk
@@ -150,10 +150,6 @@ PRODUCT_COPY_FILES += \
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Dexpreopt
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
-
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8998 \
