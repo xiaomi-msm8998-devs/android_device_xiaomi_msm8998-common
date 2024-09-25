0ff0f45100cc115c931867c4ff21ca517f3d4b92
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    msm8998-common: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- msm8998.mk
+++ msm8998.mk
@@ -382,7 +382,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb-service.qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
