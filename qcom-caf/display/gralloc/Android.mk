207ef46bef407e9a0c3216c917d172d30066d1ff
EndCredits <endcredits@crepuscular-aosp.icu>
2024-11-19 12:39:42 +0800

    Convert libgrallocutils and gpu_tonemapper to blueprint
    * Support ELF check for some blobs
    
    Change-Id: I11dec853d8cfaddb01ba96a2e76be38fe2d52073
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- qcom-caf/display/gralloc/Android.mk
+++ qcom-caf/display/gralloc/Android.mk
@@ -46,28 +46,3 @@ ifeq ($(call is-board-platform-in-list, msm8909), true)
 endif
 
 include $(BUILD_SHARED_LIBRARY)
-
-#libgrallocutils
-include $(CLEAR_VARS)
-LOCAL_MODULE                  := libgrallocutils
-LOCAL_VENDOR_MODULE           := true
-LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-ifeq ($(TARGET_KERNEL_VERSION), 4.14)
-  ifneq ($(LIBION_HEADER_PATH_WRAPPER), )
-    include $(LIBION_HEADER_PATH_WRAPPER)
-    LOCAL_C_INCLUDES += $(LIBION_HEADER_PATHS)
-  else
-    LOCAL_C_INCLUDES += system/core/libion/include
-    LOCAL_C_INCLUDES += system/core/libion/kernel-headers/
-  endif
-endif
-ifeq ($(TARGET_KERNEL_VERSION), 4.19)
-LOCAL_C_INCLUDES              += $(LIBION_HEADER_PATHS)
-endif
-LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl android.hardware.graphics.common@1.1
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"grallocutils\" -Wno-sign-conversion -Wno-enum-enum-conversion
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
-LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
-include $(BUILD_SHARED_LIBRARY)
