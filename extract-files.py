673d9a1ab0996751531bc0a4860f320be6e4b729
Nanhumly <admin@warbler.cc>
2025-03-09 13:10:19 +0000

    msm8998-common: fix vendor.qti.hardware.tui_comm@1.0 ELF checks
    Change-Id: I04e7cce9ddb541513b182fec93ce661305eed3cf

--- extract-files.py
+++ extract-files.py
@@ -41,6 +41,7 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.0',
         'vendor.qti.hardware.qccsyshal@1.0',
         'vendor.qti.hardware.qccvndhal@1.0',
+        'vendor.qti.hardware.tui_comm@1.0',
     ): lib_fixup_vendor_suffix,
     (
     ): lib_fixup_remove,
