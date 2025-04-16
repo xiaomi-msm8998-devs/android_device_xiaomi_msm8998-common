4bc7948b4c86de22d89d1a5d703f6bfb705324d2
Nanhumly <admin@warbler.cc>
2025-04-16 10:25:39 +0000

    msm8998-common: Update PD mapper blobs from helios
    Change-Id: Ic416cfda6529a928aad95b1173d226a5ab30033e

--- config.fs
+++ config.fs
@@ -40,6 +40,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[system/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [vendor/bin/imsdaemon]
 mode: 0755
 user: AID_RADIO
