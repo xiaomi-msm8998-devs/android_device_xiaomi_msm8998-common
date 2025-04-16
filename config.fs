eb336db1d61f4b9dd22b301dd03c5bd8f497a074
Nanhumly <admin@warbler.cc>
2025-04-16 10:14:45 +0000

    msm8998-common: Update Peripheral manager blobs from helios
    Change-Id: Id7c43645febf57dbe1889077b84ccb3f0660cccf

--- config.fs
+++ config.fs
@@ -28,6 +28,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[system/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [vendor/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
