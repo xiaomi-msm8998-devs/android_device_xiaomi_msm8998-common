38208444bb38d1b52fc5ade2709e09be3f942951
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    msm8998-common: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- msm8998.mk
+++ msm8998.mk
@@ -254,7 +254,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
 
 # Media
 PRODUCT_COPY_FILES += \
