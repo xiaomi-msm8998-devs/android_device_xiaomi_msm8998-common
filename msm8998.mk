3a944e54f27cecacae0c7b2efd8cd3df3d291f05
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    msm8998-common: Drop UFFD flags
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- msm8998.mk
+++ msm8998.mk
@@ -251,9 +251,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
